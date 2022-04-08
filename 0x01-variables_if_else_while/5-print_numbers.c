5d66c786548bd6f7c80d8e9fbdb84c1e621bd930
Solomon Eniola Adegoke <souceking@gmail.com>
2022-04-08 13:57:07 -0700

    Numbers Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main 
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
