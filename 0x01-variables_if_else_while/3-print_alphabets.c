c2f54ecd9c822f5353733e8474dfdd76cde6f8fa
Solomon Eniola Adegoke <souceking@gmail.com>
2022-04-08 15:02:26 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
- * main 
- *
- * Return: Always (Success)
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
-	char c;
+	char ch = 'a';
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (ch <= 'z')
 	{
-		putchar(c);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
-
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
-	}
-
 	putchar('\n');
-
 	return (0);
 }
