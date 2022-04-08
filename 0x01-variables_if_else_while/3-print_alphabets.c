b3e3b2d3b8a8ede3c7f33a95fa4ee1dd08b53bdc
Solomon Eniola Adegoke <souceking@gmail.com>
2022-04-08 13:36:09 -0700

    Alphabet - Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main 
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
