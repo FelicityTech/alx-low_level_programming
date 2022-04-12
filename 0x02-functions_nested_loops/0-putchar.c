cba9864b5e614720518ffb7b2101c0f60b39cf0e
Solomon Eniola Adegoke <souceking@gmail.com>
2022-04-12 10:58:01 -0700

    _putchar-Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
 
 int main(void)
 {
-    printf("_putchar");
-    
-    return(0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
