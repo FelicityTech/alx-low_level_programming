009f01c430d179d886758a3f7b7ddd3656beaa7f
Solomon Eniola Adegoke <souceking@gmail.com>
2022-04-12 16:05:21 -0700

    Task 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,43 @@
 
 void print_times_table(int n)
 {
+    int a = 0, rep, b;
 
-int a = 0, rep, b;
+    if (n < 0 || n > 15)
+        return;
 
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
-	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
-		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-	a++;
-}
+    while (a <= n)
+    {
+        for (b = 0; b <= n; b++)
+        {
+            rep = a * b;
+            if (b == 0)
+                _putchar('0' + rep);
+            else if (rep < 10)
+            {
+                _putchar(' ');
+                _putchar(' ');
+                _putchar('0' + rep);
+            }
+            else if (rep < 100)
+            {
+                _putchar(' ');
+                _putchar('0' + rep / 10);
+                _putchar('0' + rep % 10);
+            }
+            else
+            {
+                _putchar('0' + rep / 100);
+                _putchar('0' + (rep - 100) / 10);
+                _putchar('0' + rep % 10);
+            }
+            if (b < n)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+        a++;
+    }
 }
