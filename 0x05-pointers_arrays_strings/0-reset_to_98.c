d25cee7dc4716a4df87d219642b7f6ed80d3246f
Solomon Eniola Adegoke <souceking@gmail.com>
2022-04-20 13:27:57 -0700

    98 Battery st - Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
-#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * reset_to_98 - reset argument to 98 
- ​ * @n: pointer 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​reset_to_98​(​int​ *n) 
- ​{ 
-  
- ​        *n = ​98​; 
-  
- ​}
+#include "main.h"
+
+/**
+ *  * reset_to_98 - takes a pointer and updates the value
+ *   *@n:input
+ *    * Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
