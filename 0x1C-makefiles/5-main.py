f37e4dfc6c55185d3d958664e0d9f636a4372320
Solomon Eniola Adegoke <souceking@gmail.com>
2022-09-17 07:02:50 -0700

    Island Perimeter

--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+"""
+5-main
+"""
+island_perimeter = __import__('5-island_perimeter').island_perimeter
 
+if __name__ == "__main__":
+    grid = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+            ]
 
-def island_perimeter(grid):
-        """Return the perimiter of an island.
-
-            The grid represents water by 0 and land by 1.
-
-                Args:
-                        grid (list): A list of list of integers representing an island.
-                            Returns:
-                                    The perimeter of the island defined in grid.
-                                        """
-                                            width = len(grid[0])
-                                                height = len(grid)
-                                                    edges = 0
-                                                        size = 0
-
-                                                            for i in range(height):
-                                                                        for j in range(width):
-                                                                                        if grid[i][j] == 1:
-                                                                                                            size += 1
-                                                                                                                            if (j > 0 and grid[i][j - 1] == 1):
-                                                                                                                                                    edges += 1
-                                                                                                                                                                    if (i > 0 and grid[i - 1][j] == 1):
-                                                                                                                                                                                            edges += 1
-                                                                                                                                                                                                return size * 4 - edges * 2
+    print(island_perimeter(grid))
